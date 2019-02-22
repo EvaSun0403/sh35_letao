a824ce7d454e104ec075a3c2c53a0c1b255caf98
EvaSun0403 <evasun0403@163.com>
2019-02-22 10:25:03 +0800

    login.html静态页面完成

---
+++ public/back/lib/bootstrap-validator/js/login.js
@@ -0,0 +1,47 @@
+// 使用表单校验插件
+/*
+   * 1. 进行表单校验配置
+   *    校验要求:
+   *        (1) 用户名不能为空, 长度为2-6位
+   *        (2) 密码不能为空, 长度为6-12位
+   * */
+
+$(function(){
+
+    $('#form').bootstrapValidator({
+        //2. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        //指定校验字段
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    stringLength: {
+                        min:2,
+                        max:6,
+                        messsage: '用户名长度要在2-6位之间'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min:6,
+                        max:12,
+                        message: '密码长度要在6-12位之间'
+                    }
+                }
+            }
+        }
+    })
+
+});
