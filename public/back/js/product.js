4eb4317edd1ba0e4a4e130e85216d9eff1d31e42
EvaSun0403 <evasun0403@163.com>
2019-02-25 23:29:58 +0800

    二级分类 和 商品管理 完成

---
+++ public/back/js/product.js
@@ -0,0 +1,207 @@
+$(function () {
+    var currentPage = 1;
+    var pageSize = 5;
+    var picArr = [];
+
+    //1.渲染
+    render();
+
+    function render() {
+        $.ajax({
+            type: 'get',
+            url: '/product/queryProductDetailList',
+            data: {
+                page: currentPage,
+                pageSize: pageSize
+            },
+            dataType: 'json',
+            success: function (info) {
+                console.log(info);
+                var htmlStr = template('productTpl', info);
+                $('tbody').html(htmlStr);
+
+                //2.分页功能
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: currentPage,
+                    totalPages: Math.ceil(info.total / info.size),
+                    onPageClicked: function (a, b, c, page) {
+                        currentPage = page;
+                        render();
+                    }
+                })
+            }
+        })
+    }
+
+    //3.点击添加商品 展示模态框
+    $('#addBtn').on('click', function () {
+        $('#addModal').modal('show');
+        //发送ajax 动态渲染下拉框选项
+        $.ajax({
+            type: 'get',
+            url: '/category/querySecondCategoryPaging',
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            dataType: 'json',
+            success: function (info) {
+                console.log(info);
+                var htmlStr = template('listTpl', info);
+                $('#brand').html(htmlStr);
+
+            }
+        })
+    })
+
+    //4.点击下拉框选项，让按钮改变文字 把id赋值给隐藏域
+    $('#brand').on('click', 'a', function () {
+        var txt = $(this).text();
+        $('#dropdownText').text(txt);
+
+        var id = $(this).data('id');
+        $('[name="brandId"]').val(id);
+
+        //手动设置校验状态
+        $('#form').data('bootstrapValidator').updateStatus('brandId','VALID');
+    })
+
+    // <img id="picImg" width="100" src="./images/none.png" alt="">
+    //5.图片上传
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            // console.log(data);
+            //获取上传图片的信息;
+            var picObj = data.result;
+            var picSrc = picObj.picAddr;
+
+            picArr.unshift(picObj);
+            console.log(picArr);
+            
+            //把图片追加到 imgBox最前面
+            $('#imgBox').prepend('<img width="100" src="' + picSrc + '" alt="">');
+
+            if (picArr.length > 3) {
+                //删除最后一张
+                picArr.pop();
+                $('#imgBox img:last-of-type').remove();
+                // console.log(picArr);
+            }
+            if(picArr.length === 3){
+                $('#form').data('bootstrapValidator').updateStatus('picStatus','VALID');
+            }
+        }
+    })
+
+    //6.初始化 表单校验功能
+    $('#form').bootstrapValidator({
+        excluded: [],
+        //2. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        //3.指定校验字段
+        fields:{
+            brandId:{
+                validators:{
+                    notEmpty:{
+                        message:'请选择二级分类'
+                    }
+                }
+            },
+            proName:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入商品名称'
+                    }
+                }
+            },
+            proDesc:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入商品描述'
+                    }
+                }
+            },
+            num:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入商品库存'
+                    },
+                    //必须是非0开头的数字
+                    regexp: {
+                        regexp: /^[1-9]\d+$/,
+                        message: '库存商品必须是非0开头的数字'
+                    }
+                }
+            },
+            size:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入商品尺码'
+                    },
+                    regexp: {
+                        regexp: /^\d{2}-\d{2}$/,
+                        message: '尺码格式必须是 xx-xx 例如：32-44'
+                    }
+                }
+            },
+            oldPrice:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入商品原价'
+                    }
+                }
+            },
+            price:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入商品现价'
+                    }
+                }
+            },
+            picStatus:{
+                validators:{
+                    notEmpty:{
+                        message:'请上传3张图片'
+                    }
+                }
+            },
+        }
+    })
+
+    //7.注册表单校验成功事件 发送ajax
+    $('#form').on('success.form.bv',function( e ){
+        e.preventDefault();
+        console.log(picArr);
+        var paramsStr = $('#form').serialize();
+        paramsStr += '&picArr='+ JSON.stringify(picArr)
+        
+        $.ajax({
+            type:'post',
+            url:'/product/addProduct',
+            data:paramsStr,
+            dataType:'json',
+            success:function( info ){
+                // console.log(info);
+               if(info.success){
+                currentPage = 1;
+                render();
+
+                $('#addModal').modal('hide');
+
+                $('#form').data('bootstrapValidator').resetForm(true);
+
+                $('#dropdownText').text('请选择二级分类');
+
+                $('#imgBox img').remove();
+               } 
+            }
+        })
+    })
+
+})
--- public/back/js/second.js
+++ public/back/js/second.js
@@ -1,30 +1,30 @@
 
-$(function(){
+$(function () {
     var currentPage = 1;
     var pageSize = 5;
 
     render();
-    function render(){
+    function render() {
         $.ajax({
-            type:'get',
-            url:'/category/querySecondCategoryPaging',
-            data:{
+            type: 'get',
+            url: '/category/querySecondCategoryPaging',
+            data: {
                 page: currentPage,
                 pageSize: pageSize
             },
             dataType: 'json',
-            success:function( info ){
+            success: function (info) {
                 console.log(info);
-                
-                var htmlstr = template('secondTpl',info);
+
+                var htmlstr = template('secondTpl', info);
                 $('tbody').html(htmlstr);
 
                 // 分页插件
                 $('#paginator').bootstrapPaginator({
-                    bootstrapMajorVersion:3,
+                    bootstrapMajorVersion: 3,
                     currentPage: currentPage,
-                    totalPages: Math.ceil(info.total/info.size),
-                    onPageClicked:function(a,b,c,page){
+                    totalPages: Math.ceil(info.total / info.size),
+                    onPageClicked: function (a, b, c, page) {
                         currentPage = page;
                         render();
                     }
@@ -34,30 +34,113 @@ $(function(){
     }
 
     //2.点击添加按钮 模态框显示
-    $('#addBtn').on('click',function(){
-        $('#addTpl').modal('show');
+    $('#addBtn').on('click', function () {
+        $('#addModal').modal('show');
         $.ajax({
-            type:'get',
-            url:'/category/queryTopCategoryPaging',
-            data:{
-                page:1,
-                pageSize:100
+            type: 'get',
+            url: '/category/queryTopCategoryPaging',
+            data: {
+                page: 1,
+                pageSize: 100
             },
-            dataType:'json',
-            success:function(info){
-                var htmlstr = template('categoryTpl',info);
+            dataType: 'json',
+            success: function (info) {
+                var htmlstr = template('categoryTpl', info);
                 $('#category').html(htmlstr);
             }
         })
     })
 
     //3.点击下拉框事件
-    $('.dropdown-menu').on('click','a',function(){
+    $('.dropdown-menu').on('click', 'a', function () {
         var txt = $(this).text();
-        console.log(txt);
+        // console.log(txt);
         $('#dropdownText').text(txt);
-        
+        var id = $(this).data('id');
+        //把选择的分类名id  赋值给隐藏域
+        $('[name="categoryId"]').val(id);
+        //需要手动更改 校验状态
+        $('#form').data('bootstrapValidator').updateStatus('categoryId','VALID');
     })
 
+    //4.上传图片
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            // console.log(e);   
+            // console.log(data);
+            // console.log(data.result.picAddr);
+            var src = data.result.picAddr;
+            $('#picImg').attr('src', src);
+            //把图片地址设置给隐藏域
+            $('[name="brandLogo"]').val(src);
+            //手动更改 校验状态
+            $('#form').data('bootstrapValidator').updateStatus('brandLogo','VALID');
+        }
+
+    })
+
+    //5.配置表单校验
+    $('#form').bootstrapValidator({
+        //让隐藏域也校验
+        excluded: [],
+        //2. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields:{
+            categoryId:{
+                validators:{
+                    notEmpty:{
+                        message:'请选择一级分类'
+                    }
+                }
+            },
+            brandName:{
+                validators:{
+                    notEmpty:{
+                        message:'请输入二级分类名称'
+                    }
+                }
+            },
+            brandLogo:{
+                validators:{
+                    notEmpty:{
+                        message:'请上传图片'
+                    }
+                }
+            }
+        }
+    });
+
+
+
+    //6.点击模态框右下角的 添加功能
+    $('#form').on('success.form.bv',function(e){
+        e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                url: '/category/addSecondCategory',
+                data: $('#form').serialize(),
+                dataType: 'json',
+                success: function (info) {
+                    console.log(info);
+                    if(info.success){
+                        currentPage = 1;
+                        render();
+
+                        $('#addModal').modal('hide');
+                        //重置表单项 内容+状态
+                        $('#form').data('bootstrapValidator').resetForm(true);
+                        //针对 一级分类的按钮 和 图片隐藏域 单独处理
+                        $('#dropdownText').text('请选择一级分类');
+                        $('#picImg ').attr('src','./images/none.png');
+                    }
+                }
+            })
+        })
 
 })
